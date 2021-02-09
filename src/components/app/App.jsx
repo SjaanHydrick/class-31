96bc4164883071b5800b2d99a668ff7a122dd3ec
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-09 14:21:12 -0800

    full rendering and functionality achieved

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,16 +4,20 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import Header from '../header/Header';
 import ShowById from '../../containers/ShowById';
 import ShowsPage from '../../containers/ShowsPage';
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={ShowsPage} />
-        <Route path="/shows/:id" component={ShowById} />
-      </Switch>
-    </Router>
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={ShowsPage} />
+          <Route path="/shows/:id" component={ShowById} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
