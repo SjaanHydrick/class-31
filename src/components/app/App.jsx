5474a0e6040262d45cea407f802cdee0207eee85
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-09 14:09:32 -0800

    App written

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import ShowById from '../../containers/ShowById';
+import ShowsPage from '../../containers/ShowsPage';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ShowsPage} />
+        <Route path="/shows/:id" component={ShowById} />
+      </Switch>
+    </Router>
+  );
 }
