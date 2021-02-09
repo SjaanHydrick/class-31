b8e46165673a17a78d1e0799c3cf635fee607173
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-09 13:54:31 -0800

    Loading written

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const Loading = () => (
+  <img src="https://media2.giphy.com/media/9nmRB7rnndLsk/source.gif" alt="loading" />
+);
+
+export default Loading;
